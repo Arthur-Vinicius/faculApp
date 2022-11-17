879ee7ac7f2b569d23dcaf08e0c13c73b3a93c7f
ArthurSouza <arthurviniciussouza906@gmail.com>
2022-11-17 11:55:16 -0400

    responsividade concluida - falta ajustes de espaçamento.

--- staticfiles/js/javascript.js
+++ staticfiles/js/javascript.js
@@ -23,17 +23,3 @@ chk.addEventListener('change', function () {
     localStorage.setItem ("dark", 1);
   }
 });
-
-// navbar
-
-var menuHolder = document.getElementById('menuHolder')
-var siteBrand = document.getElementById('siteBrand')
-function menuToggle(){
-  if(menuHolder.className === "drawMenu") menuHolder.className = ""
-  else menuHolder.className = "drawMenu"
-}
-if(window.innerWidth < 426) siteBrand.innerHTML = "MAS"
-window.onresize = function(){
-  if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
-  else siteBrand.innerHTML = "MY AWESOME WEBSITE"
-}
